e345aeee9407a801f3ff11ff3176ec9d79c70f84
Thiago Bohonos <thiago.bohonos@gmail.com>
2023-01-03 21:00:39 -0300

    Iniciando estrutura de projeto

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Teste");
+
+int quantidade = 1;
+
+Console.WriteLine(quantidade);
