b8661d407cbbcc6df07b86f18534cab1279e8949
Thiago Bohonos <thiago.bohonos@gmail.com>
2023-01-03 21:03:03 -0300

    Modificando o arquivo RAEDME.md

--- Program.cs
+++ Program.cs
@@ -1,7 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
-Console.WriteLine("Teste");
 
-int quantidade = 1;
 
-Console.WriteLine(quantidade);
---
+++ README.md
@@ -0,0 +1,1 @@
+### Curso de CSharp desenvolvido pela DIO
