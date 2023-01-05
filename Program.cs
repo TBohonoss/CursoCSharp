9ae1d03a8e008bbd3429f803fce97d641195dca7
Thiago Bohonos <thiago.bohonos@gmail.com>
2023-01-05 18:01:45 -0300

    Usando a classe Pessoa

--- Program.cs
+++ Program.cs
@@ -1,4 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using ExemploFundamentos.Models;
+
+Pessoa pessoa1 = new Pessoa();
+
+pessoa1.Nome = "Thiago Bohonos";
+pessoa1.Idade = 31;
+pessoa1.Apresentar();
 
 
